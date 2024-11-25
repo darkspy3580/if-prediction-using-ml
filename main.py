7a23f627c935bf2348d8227cafdf6d4ceb207b05
darkspy3580 <91835862+darkspy3580@users.noreply.github.com>
2024-11-25 15:42:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,12 @@ try:
     page_bg_img = f"""
     <style>
     .stApp {{
-      
+      background-color: #1E1E1E; 
+        background-image: url("data:image/png;base64,{image_base64}");
+        background-position: left;
+        padding: 3rem;
+        border-radius: 10px;
+        text-align: center;  
     }}
     </style>
     """
