29714f39da69c5d43519c14a359f0875ec8a7463
darkspy3580 <91835862+darkspy3580@users.noreply.github.com>
2024-11-25 15:54:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ try:
       background-color: #1E1E1E; 
       background-image: url("data:image/png;base64,{image_base64}");
       background-position: center top;  /* Adjusts the position */
-      background-size: 100%;  /* Zooms out the image */
+      background-size: 50%;  /* Zooms out the image */
       background-repeat: no-repeat;
       padding: 3rem;
       border-radius: 10px;
